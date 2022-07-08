0307db0a8942b03a6829950799e03a07ed707dd1
Mario <mariovladimirovfs@gmail.com>
2022-07-08 09:54:19 +0300

    upload

---
+++ 05. Basic Syntax, Conditional Statements and Loops/07. Numbers From 1 to 5.js
@@ -0,0 +1,12 @@
+function numbersFrom1to5() {
+  //for (let i = 1; i <=5; i++){
+  //      console.log(i);
+  //}
+
+  let i = 1;
+  while (i <= 5) {
+    console.log(i);
+    i++;
+  }
+}
+numbersFrom1to5();
