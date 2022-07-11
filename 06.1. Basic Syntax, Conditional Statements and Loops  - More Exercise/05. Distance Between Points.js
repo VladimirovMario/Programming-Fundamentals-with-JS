ac2596fb5e9f2327ac09c3117145b08c3c47cb6e
Mario <mariovladimirovfs@gmail.com>
2022-07-11 10:20:04 +0300

    upload

---
+++ 06.1. Basic Syntax, Conditional Statements and Loops  - More Exercise/05. Distance Between Points.js
@@ -0,0 +1,8 @@
+function distanceBetweenPoints(x1, y1, x2, y2) {
+  let x = Math.pow(x2 - x1, 2);
+  let y = Math.pow(y2 - y1, 2);
+  let result = Math.sqrt(x + y);
+  console.log(result);
+}
+distanceBetweenPoints(2, 4, 5, 0);
+distanceBetweenPoints(2.34, 15.66, -13.55, -2.9985);
