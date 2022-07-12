d44a2707a3efeaddabe15bd25905b8c1f52fc9e4
Mario <mariovladimirovfs@gmail.com>
2022-07-12 09:44:37 +0300

    upload

---
+++ 08. Data Types and Variables - Lab/01. EchoType.js
@@ -0,0 +1,12 @@
+function echoType(params) {
+  if (typeof params == `string` || typeof params === `number`) {
+    console.log(typeof params);
+    console.log(params);
+  } else {
+    console.log(typeof params);
+    console.log(`Parameter is not suitable for printing`);
+  }
+}
+echoType("Hello, JavaScript!");
+echoType(18);
+echoType(null);
