3208004e540fcae7c9c9f88098c39b414ef3845e
Mario <mariovladimirovfs@gmail.com>
2022-07-18 09:16:43 +0300

    upload

---
+++ 11. Arrays - Lab/01. Sum First And Last Array Elements.js
@@ -0,0 +1,9 @@
+function sumFirstAndLastArrayElements(input) {
+  const firstElement = input[0];
+  const lastElement = input[input.length - 1];
+
+  console.log(firstElement + lastElement);
+}
+sumFirstAndLastArrayElements([20, 30, 40]);
+sumFirstAndLastArrayElements([10, 17, 22, 33]);
+sumFirstAndLastArrayElements([11, 58, 69]);
