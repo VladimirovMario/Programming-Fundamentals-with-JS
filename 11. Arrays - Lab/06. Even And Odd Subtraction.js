48002a21957dca3bff63f1efda9f2332ac0570ff
Mario <mariovladimirovfs@gmail.com>
2022-07-19 08:51:24 +0300

    upload

---
+++ 11. Arrays - Lab/06. Even And Odd Subtraction.js
@@ -0,0 +1,16 @@
+function evenAndOddSubtraction(array) {
+  let evenSum = 0;
+  let oddSum = 0;
+  for (let i = 0; i < array.length; i++) {
+    let currentNum = Number(array[i]);
+    if (currentNum % 2 === 0) {
+      evenSum += currentNum;
+    } else {
+      oddSum += currentNum;
+    }
+  }
+  console.log(evenSum - oddSum);
+}
+evenAndOddSubtraction([1, 2, 3, 4, 5, 6]);
+evenAndOddSubtraction([3, 5, 7, 9]);
+evenAndOddSubtraction([2, 4, 6, 8, 10]);
