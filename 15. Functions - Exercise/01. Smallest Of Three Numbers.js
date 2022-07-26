7f01dd0ee8226d05106791e7b7f02c037ed0c04f
Mario <mariovladimirovfs@gmail.com>
2022-07-26 09:06:03 +0300

    upload

---
+++ 15. Functions - Exercise/01. Smallest Of Three Numbers.js
@@ -0,0 +1,8 @@
+function smallestOfThreeNumbers(firstNum, secondNum, thirdNum) {
+  function findSmallest(firstNum, secondNum, thirdNum) {
+    return Math.min(firstNum, secondNum, thirdNum);
+  }
+  console.log(findSmallest(firstNum, secondNum, thirdNum));
+}
+// smallestOfThreeNumbers(2, 5, 3);
+smallestOfThreeNumbers(600, 342, 123);
