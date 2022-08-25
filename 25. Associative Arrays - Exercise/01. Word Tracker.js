f85278a64a39a89803e95a48278c2243a3fee7ca
Mario <mariovladimirovfs@gmail.com>
2022-08-25 11:36:57 +0300

    upload

---
+++ 25. Associative Arrays - Exercise/01. Word Tracker.js
@@ -0,0 +1,56 @@
+function wordTracker(array) {
+  let words = array.shift().split(` `);
+  let tracker = {};
+
+  for (const word of words) {
+    tracker[word] = 0;
+  }
+
+  for (const currWord of array) {
+    if (tracker.hasOwnProperty(currWord)) {
+      tracker[currWord]++; // по този начин може да увеличим
+      // стойността на пропъртито с едно всеки път вместо да правим 'counter'
+    }
+  }
+  Object.entries(tracker)
+    .sort((a, b) => b[1] - a[1])
+    .forEach((word) => console.log(word[0], `-`, word[1]));
+}
+wordTracker([
+  "this sentence",
+  "In",
+  "this",
+  "sentence",
+  "you",
+  "have",
+  "to",
+  "count",
+  "the",
+  "occurances",
+  "of",
+  "the",
+  "words",
+  "this",
+  "and",
+  "sentence",
+  "because",
+  "this",
+  "is",
+  "your",
+  "task",
+]);
+console.log(`---`);
+wordTracker([
+  "is the",
+  "first",
+  "sentence",
+  "Here",
+  "is",
+  "another",
+  "the",
+  "And",
+  "finally",
+  "the",
+  "the",
+  "sentence",
+]);
