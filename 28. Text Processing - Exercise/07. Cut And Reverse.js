1275766326f34ef24cd3f8aa0182dc1386fc1df4
Mario <mariovladimirovfs@gmail.com>
2022-08-31 10:34:07 +0300

    upload

---
+++ 28. Text Processing - Exercise/07. Cut And Reverse.js
@@ -0,0 +1,18 @@
+function cutAndReverse(string) {
+  let firstHalf = string
+    .substring(0, string.length / 2)
+    .split(``)
+    .reverse()
+    .join(``);
+
+  let secondHalf = string
+    .substring(string.length / 2)
+    .split(``)
+    .reverse()
+    .join(``);
+  console.log(firstHalf);
+  console.log(secondHalf);
+}
+cutAndReverse("tluciffiDsIsihTgnizamAoSsIsihT");
+console.log(`---`);
+cutAndReverse("sihToDtnaCuoYteBIboJsihTtAdooGoSmI");
